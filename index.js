661960e0b97508c79397dbf197a99fdd6fc8603d
chiuannica <chiuannica@gmail.com>
2019-08-24 22:07:38 -0400

    created circle graph and added stats for m,f,b

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function Stat(className, percentage) {
   this.className = className,
   this.percentage = percentage
@@ -11,13 +12,12 @@ function Stat(className, percentage) {
       getElement.style.width = percentagePerSecond * timePassed + "%"
       if (timePassed >= totalTime) {
         clearInterval(timer)
-        getElement.innerHTML = "<p>" + percentage + "%</p>"
+        getElement.innerHTML = "<p class='stat-percent'>" + percentage + "%</p>"
         return 
       }
     })
   }
 }
-
 let maleStat = new Stat("male", 91.7)
 let femaleStat = new Stat("female", 7.9)
 let nonBStat = new Stat("non-binary", 1.2)
@@ -25,7 +25,6 @@ let nonBStat = new Stat("non-binary", 1.2)
 let maleStatComp = new Stat("male-comp", 67.7)
 let nonBStatComp = new Stat("nonb-comp", 59.1)
 let femaleStatComp = new Stat("female-comp", 53.4)
-
 
 maleStat.makeWider()
 femaleStat.makeWider()
@@ -35,6 +34,34 @@ maleStatComp.makeWider()
 nonBStatComp.makeWider()
 femaleStatComp.makeWider()
 
+let genderOfAllRespondents = document.getElementById("gender-chart")
+let size = 500
+genderOfAllRespondents.width = size
+genderOfAllRespondents.height = size
+var ctxGender = genderOfAllRespondents.getContext("2d");
+
+function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color) {
+  ctx.fillStyle = color
+  ctx.beginPath()
+  ctx.moveTo(centerX, centerY)
+  ctx.arc(centerX, centerY, radius, startAngle, endAngle)
+  ctx.closePath()
+  ctx.fill()
+}
+function calcAngle(percent) {
+  let startAngle = (percent / 100) * 2 * Math.PI
+  return startAngle
+}
+let genderChartPercentages = [91.7, 7.9, 1.2]
+const genderChartPercentagesPieSlice = genderChartPercentages.map(percent => calcAngle(percent))
+
+let endMale = genderChartPercentagesPieSlice[0]
+let endFemale = endMale + genderChartPercentagesPieSlice[1]
+let endNonB = endFemale + genderChartPercentagesPieSlice[2] 
+
+drawPieSlice(ctxGender, size/2, size/2, size/2, 0, endMale, '#EC9F48')
+drawPieSlice(ctxGender, size/2, size/2, size/2, endMale, endFemale, '#ab9F10')
+drawPieSlice(ctxGender, size/2, size/2, size/2, endFemale, endNonB, '#ae2e31')
 
 
 
